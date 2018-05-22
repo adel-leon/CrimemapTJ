4aa3868e0d8b888d77280e99a22c04754afc60b5
Adel Leon <ic.adel.leon@gmail.com>
2018-05-21 21:42:20 -0700

    Tidy up mapbox script

--- public/javascripts/mapbox.js
+++ public/javascripts/mapbox.js
@@ -1,71 +1,101 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaWMtYWRlbCIsImEiOiJjajlxNmh3Z2c1dGo0MzJsc2g4dmtwemtmIn0.OaHM_ansPzWj1GehUGluMw';
+var crimeData;
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v9',
+    style: 'mapbox://styles/mapbox/bright-v9',
     center: [-116.94, 32.48],
     zoom: 10
 });
 
-var crimeData;
+function mapTweaking() {
+    //dev tweaking
+    var blur = document.getElementById('blur');
+    var weight = document.getElementById('weight');
+    var intensity = document.getElementById('intensity');
+    var radius = document.getElementById('radius');
+    var opacity = document.getElementById('opacity');
+    
+    var blur_value = document.getElementById('blur-value');
+    var weight_value = document.getElementById('weight-value');
+    var intensity_value = document.getElementById('intensity-value');
+    var radius_value = document.getElementById('radius-value');
+    var opacity_value = document.getElementById('opacity-value');
+    
+    var points_layer = document.getElementById('points');
+    var heat_layer = document.getElementById('heat');
+    
+    var zoom = document.getElementById('zoom');
+    
+    
+    // blur.addEventListener('input', function() {
+    //     map.setPaintProperty('earthquakes-heat','heatmap-blur',parseInt(blur.value,10));
+    // });
+    
+    points.addEventListener('input', function() {
+        if (points.checked === true) {
+            map.setLayoutProperty('earthquakes-point','visibility','visible');
+        } else {
+            map.setLayoutProperty('earthquakes-point','visibility','none');
+        }
+    });
+    
+    heat.addEventListener('input', function() {
+        if (heat.checked === true) {
+            map.setLayoutProperty('earthquakes-heat','visibility','visible');
+        } else {
+            map.setLayoutProperty('earthquakes-heat','visibility','none');
+        }
+    });
+    
+    weight.addEventListener('input', function() {
+        map.setPaintProperty('earthquakes-heat','heatmap-weight',parseFloat(weight.value,10));
+        weight_value.innerText = weight.value;
+    });
+    
+    intensity.addEventListener('input', function() {
+        map.setPaintProperty('earthquakes-heat','heatmap-intensity',parseFloat(intensity.value,10));
+        intensity_value.innerText = intensity.value;
+    });
+    
+    radius.addEventListener('input', function() {
+        map.setPaintProperty('earthquakes-heat','heatmap-radius',parseFloat(radius.value,10));
+        radius_value.innerText = radius.value;
+    });
+    
+    opacity.addEventListener('input', function() {
+        map.setPaintProperty('earthquakes-heat','heatmap-opacity',parseFloat(opacity.value,10));
+        opacity_value.innerText = opacity.value;
+    });
+}
 
-//dev tweaking
-var blur = document.getElementById('blur');
-var weight = document.getElementById('weight');
-var intensity = document.getElementById('intensity');
-var radius = document.getElementById('radius');
-var opacity = document.getElementById('opacity');
+function getCrimeData() {
+    $.ajax({
+        url: "http://localhost:3000/API/crimes",
+        contentType: "application/json",
+        type: "GET",
+        success: function(response){
+            console.log("Success");
+        },
+        error: function(error,status){
+            console.error(error);
+        }
+    }).done(function (data) {
+        crimeData = data[0];
+        if (map.loaded()) {
+            addMapData();
+        }
 
-var blur_value = document.getElementById('blur-value');
-var weight_value = document.getElementById('weight-value');
-var intensity_value = document.getElementById('intensity-value');
-var radius_value = document.getElementById('radius-value');
-var opacity_value = document.getElementById('opacity-value');
+    });    
+}
 
-var points_layer = document.getElementById('points');
-var heat_layer = document.getElementById('heat');
-
-var zoom = document.getElementById('zoom');
-
-$.ajax({
-    url: "http://localhost:3000/API/crimes",
-    contentType: "application/json",
-    type: "GET",
-    async: false,
-    success: function(response){
-        console.log("Success");
-    },
-    error: function(error,status){
-        console.error(error);
-    }
-}).done(function (data) {
-    console.log(data);
-    crimeData = data[0];
-    console.log(crimeData);
-});
-
-
-// var data = (function() {
-//     var json = null;
-//     $.ajax({
-//         'async': false,
-//         'global': false,
-//         'url': "/incidencia_Tijuana_2018_carto.json",
-//         'dataType': "json",
-//     }).done(function (data) {
-//         json = data;
-//     });
-//     return json;
-// })();
-
-map.on('load', function() {
+function addMapData() {
     // Add a geojson point source.
     // Heatmap layers also work with a vector tile source.
     map.addSource('earthquakes', {
         "type": "geojson",
         "data": crimeData
-       
     });
-
+    
     map.addLayer({
         "id": "earthquakes-heat",
         "type": "heatmap",
@@ -120,8 +150,8 @@ map.on('load', function() {
             //     9, 0
             // ],
         }
-    }, 'waterway-label');
-
+    },"water_label"); //water label is the first label layer for the current style
+    
     map.addLayer({
         "id": "earthquakes-point",
         "type": "circle",
@@ -171,62 +201,13 @@ map.on('load', function() {
             //     8, 1
             // ]
         }
-    }, 'waterway-label');
+    },"water_label");
+}
+
+$( document ).ready(function() {
+    mapTweaking();
+    getCrimeData();
+    
 });
 
-// add markers to map
-// data.features.forEach(function(marker) {
 
-//     // create a HTML element for each feature
-//     var el = document.createElement('div');
-//     el.className = 'marker';
-  
-//     // make a marker for each feature and add to the map
-//     new mapboxgl.Marker(el)
-//     .setLngLat(marker.geometry.coordinates)
-//     .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-//     .setHTML('<h3>' + marker.properties.DELITO + '</h3><p>' + marker.properties.MUNICIPIO + '</p>'))
-//     .addTo(map);
-//   });
-  
-  
-
-// blur.addEventListener('input', function() {
-//     map.setPaintProperty('earthquakes-heat','heatmap-blur',parseInt(blur.value,10));
-// });
-
-points.addEventListener('input', function() {
-    if (points.checked === true) {
-        map.setLayoutProperty('earthquakes-point','visibility','visible');
-    } else {
-        map.setLayoutProperty('earthquakes-point','visibility','none');
-    }
-});
-
-heat.addEventListener('input', function() {
-    if (heat.checked === true) {
-        map.setLayoutProperty('earthquakes-heat','visibility','visible');
-    } else {
-        map.setLayoutProperty('earthquakes-heat','visibility','none');
-    }
-});
-
-weight.addEventListener('input', function() {
-    map.setPaintProperty('earthquakes-heat','heatmap-weight',parseInt(weight.value,10));
-    weight_value.innerText = weight.value;
-});
-
-intensity.addEventListener('input', function() {
-    map.setPaintProperty('earthquakes-heat','heatmap-intensity',parseInt(intensity.value,10));
-    intensity_value.innerText = intensity.value;
-});
-
-radius.addEventListener('input', function() {
-    map.setPaintProperty('earthquakes-heat','heatmap-radius',parseInt(radius.value,10));
-    radius_value.innerText = radius.value;
-});
-
-opacity.addEventListener('input', function() {
-    map.setPaintProperty('earthquakes-heat','heatmap-opacity',parseInt(opacity.value,10));
-    opacity_value.innerText = opacity.value;
-});
