78ac4480118a9c74fbd7220b9792387034d17a07
Adel Leon <ic.adel.leon@gmail.com>
2018-06-02 14:43:21 -0700

    Create crime clustering layer

--- public/javascripts/mapbox.js
+++ public/javascripts/mapbox.js
@@ -28,42 +28,42 @@ function mapTweaking() {
     
     
     // blur.addEventListener('input', function() {
-    //     map.setPaintProperty('earthquakes-heat','heatmap-blur',parseInt(blur.value,10));
+    //     map.setPaintProperty('crime-heat','heatmap-blur',parseInt(blur.value,10));
     // });
     
     points.addEventListener('input', function() {
         if (points.checked === true) {
-            map.setLayoutProperty('earthquakes-point','visibility','visible');
+            map.setLayoutProperty('crime-point','visibility','visible');
         } else {
-            map.setLayoutProperty('earthquakes-point','visibility','none');
+            map.setLayoutProperty('crime-point','visibility','none');
         }
     });
     
     heat.addEventListener('input', function() {
         if (heat.checked === true) {
-            map.setLayoutProperty('earthquakes-heat','visibility','visible');
+            map.setLayoutProperty('crime-heat','visibility','visible');
         } else {
-            map.setLayoutProperty('earthquakes-heat','visibility','none');
+            map.setLayoutProperty('crime-heat','visibility','none');
         }
     });
     
     weight.addEventListener('input', function() {
-        map.setPaintProperty('earthquakes-heat','heatmap-weight',parseFloat(weight.value,10));
+        map.setPaintProperty('crime-heat','heatmap-weight',parseFloat(weight.value,10));
         weight_value.innerText = weight.value;
     });
     
     intensity.addEventListener('input', function() {
-        map.setPaintProperty('earthquakes-heat','heatmap-intensity',parseFloat(intensity.value,10));
+        map.setPaintProperty('crime-heat','heatmap-intensity',parseFloat(intensity.value,10));
         intensity_value.innerText = intensity.value;
     });
     
     radius.addEventListener('input', function() {
-        map.setPaintProperty('earthquakes-heat','heatmap-radius',parseFloat(radius.value,10));
+        map.setPaintProperty('crime-heat','heatmap-radius',parseFloat(radius.value,10));
         radius_value.innerText = radius.value;
     });
     
     opacity.addEventListener('input', function() {
-        map.setPaintProperty('earthquakes-heat','heatmap-opacity',parseFloat(opacity.value,10));
+        map.setPaintProperty('crime-heat','heatmap-opacity',parseFloat(opacity.value,10));
         opacity_value.innerText = opacity.value;
     });
 }
@@ -85,21 +85,44 @@ function getCrimeData() {
             addMapData();
         }
 
+        // add markers to map
+    // crimeData.features.forEach(function(marker) {
+        
+    //     // create a HTML element for each feature
+    //     var el = document.createElement('div');
+    //     el.className = 'marker';
+        
+    //     // make a marker for each feature and add to the map
+    //     new mapboxgl.Marker(el)
+    //     .setLngLat(marker.geometry.coordinates)
+    //     .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    //     .setHTML('<h3>' + marker.properties.DELITO + '</h3><p>' + marker.properties.MUNICIPIO + '</p>'))
+    //     .addTo(map);
+    // });
     });    
 }
 
 function addMapData() {
     // Add a geojson point source.
     // Heatmap layers also work with a vector tile source.
-    map.addSource('earthquakes', {
+    map.addSource('crime', {
         "type": "geojson",
         "data": crimeData
     });
+
+    map.addSource('crime-clustered', {
+        "type": "geojson",
+        "data": crimeData,
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 25 // Radius of each cluster when clustering points (defaults to 50)
+    });
     
+    //heatmap
     map.addLayer({
-        "id": "earthquakes-heat",
+        "id": "crime-heat",
         "type": "heatmap",
-        "source": "earthquakes",
+        "source": "crime",
         // "maxzoom": 9,
         "paint": {
             // Increase the heatmap weight based on frequency and property magnitude
@@ -151,57 +174,62 @@ function addMapData() {
             // ],
         }
     },"water_label"); //water label is the first label layer for the current style
-    
+    //crimes
     map.addLayer({
-        "id": "earthquakes-point",
-        "type": "circle",
-        "source": "earthquakes",
+        "id": "crime-point",
+        "type": "symbol",
+        "source": "crime-clustered",
+        filter: ["!has", "point_count"],
+        "layout": {
+            "icon-image": "police-15"
+        },
         // "minzoom": 7,
-        "paint": {
-            // Size circle radius by earthquake magnitude and zoom level
-            "circle-radius": [
-                "interpolate",
-                ["linear"],
-                ["zoom"],
-                7, [
-                    "interpolate",
-                    ["linear"],
-                    ["get", "mag"],
-                    1, 1,
-                    6, 4
-                ],
-                16, [
-                    "interpolate",
-                    ["linear"],
-                    ["get", "mag"],
-                    1, 5,
-                    6, 50
-                ]
-            ],
-            // Color circle by earthquake magnitude
+        
+    },"water_label"); //place layer before all map labels
+    //crime clusters
+    map.addLayer({
+        id: "crime-clusters",
+        type: "circle",
+        source: "crime-clustered",
+        filter: ["has", "point_count"],
+        paint: {
+            // Use step expressions (https://www.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+            // with three steps to implement three types of circles:
+            //   * Blue, 20px circles when point count is less than 100
+            //   * Yellow, 30px circles when point count is between 100 and 750
+            //   * Pink, 40px circles when point count is greater than or equal to 750
             "circle-color": [
-                "interpolate",
-                ["linear"],
-                ["get", "mag"],
-                1, "rgba(33,102,172,0)",
-                2, "rgb(103,169,207)",
-                3, "rgb(209,229,240)",
-                4, "rgb(253,219,199)",
-                5, "rgb(239,138,98)",
-                6, "rgb(178,24,43)"
+                "step",
+                ["get", "point_count"],
+                "#51bbd6",
+                100,
+                "#f1f075",
+                750,
+                "#f28cb1"
             ],
-            "circle-stroke-color": "white",
-            "circle-stroke-width": 1,
-            // Transition from heatmap to circle layer by zoom level
-            // "circle-opacity": [
-            //     "interpolate",
-            //     ["linear"],
-            //     ["zoom"],
-            //     7, 0,
-            //     8, 1
-            // ]
+            "circle-radius": [
+                "step",
+                ["get", "point_count"],
+                20,
+                100,
+                30,
+                750,
+                40
+            ]
         }
-    },"water_label");
+    });
+    //cluster labels
+    map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "crime-clustered",
+        filter: ["has", "point_count"],
+        layout: {
+            "text-field": "{point_count_abbreviated}",
+            "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+            "text-size": 12
+        }
+    });
 }
 
 $( document ).ready(function() {
